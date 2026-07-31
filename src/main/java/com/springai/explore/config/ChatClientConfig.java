150e99d90b87d9bb822a477ceda3bbfdce49c9fd
Yellaiah_Mekala <yellaiahmekala614@gmail.com>
2026-07-31 14:58:20 +0530

    Spring AI Stateful with ChatMemory

--- src/main/java/com/springai/explore/config/ChatClientConfig.java
+++ src/main/java/com/springai/explore/config/ChatClientConfig.java
@@ -4,6 +4,8 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,7 +14,13 @@ import java.util.List;
 @Configuration
 public class ChatClientConfig {
 
-
+    @Bean
+    public ChatMemory chatMemory(JdbcChatMemoryRepository jdbcChatMemoryRepository){
+       return  MessageWindowChatMemory.builder()
+                .maxMessages(5)
+                .chatMemoryRepository(jdbcChatMemoryRepository)
+                .build();
+    }
 
     @Bean
     public ChatClient chatClient(ChatClient.Builder chatClientBuilder,ChatMemory chatMemory){
@@ -20,7 +28,6 @@ public class ChatClientConfig {
         SimpleLoggerAdvisor loggerAdvisor=  new SimpleLoggerAdvisor();
         MessageChatMemoryAdvisor memoryAdvisor =  MessageChatMemoryAdvisor
               .builder(chatMemory)
-
                 .build();
 
         return  chatClientBuilder
